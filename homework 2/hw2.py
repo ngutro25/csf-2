0149596723dde6a013a69a398ea6e344bbd447ce
LogTyl02 <logtyl02@evergreen.edu>
2013-10-05 20:38:30 -0700

    first commit
    started work on homework 2, not much done yet

---
+++ homework 2/hw2.py
@@ -0,0 +1,91 @@
+# Name: Tyler Logan
+# Evergreen Login: Logtyl02
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 2
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+n = 1
+
+
+###
+### Problem 1
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 1 solution follows:"
+
+
+
+
+
+###
+### Problem 2
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 2 solution follows:"
+
+for i in range(2, 11):
+	print float(1) / float(i)
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+n = 10
+triangular = 0
+for i in ...:
+    triangular = ...
+print "Triangular number", n, "via loop:", triangular
+print "Triangular number", n, "via formula:", n*(n+1)/2
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
+
+###
+### Reflection
+###
+
+# ... Write how long this assignment took you, including doing all the readings
+# ... and tutorials linked to from the homework page. Did the readings, tutorials,
+# ... and lecture contain everything you needed to complete this assignment?

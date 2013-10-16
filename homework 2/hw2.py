7edd4deaadd2b40a573236b7041dd9df1e5bb4db
Tyler <logtyl02@evergreen.edu>
2013-10-15 22:11:21 -0700

    Wow, thanks, Git
    Apparently Git decided to crap itself and corrupt the hell out of my file. I think it tried to do some weird merging stuff, cuz I had old and new code smooshed together all up ins.
    
    Should all work now. I hope. Gonna have to fix Git on my computer now.
    
    REAL FINAL UPDATE BEFORE DUE (Prolly)

--- homework 2/hw2.py
+++ homework 2/hw2.py
@@ -9,7 +9,6 @@
 # you are done.  When you are done, running this file should compute and
 # print the answers to all the problems.
 
-from math import factorial
 from hw2_test import n
 
 
@@ -28,7 +27,6 @@ while i <= n:
 	i += 1
 	print answer
 
-
 ###
 ### Problem 2
 ###
@@ -38,7 +36,6 @@ print "Problem 2 solution follows:"
 
 for i in range(2, 11):
 	print float(1) / float(i)
-
 
 ###
 ### Problem 3
@@ -54,7 +51,6 @@ for i in range(triangular, n + 1):    # Adding 1 because we start at 0
 print "Triangular number", n, "via loop:", triangular
 print "Triangular number", n, "via formula:", n * (n + 1) / 2
 
-
 ###
 ### Problem 4
 ###
@@ -62,20 +58,11 @@ print "Triangular number", n, "via formula:", n * (n + 1) / 2
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 4 solution follows:"
 
-<<<<<<< HEAD
 n = 10
-fact = 0
-for i in range(fact, n):
-    fact = fact * i
-    fact += 1
-    print fact
-=======
 answer = 1
 for i in range(1, n+1):
 	answer *= i
 	print answer
->>>>>>> all finished up~
-
 
 ### 
 ### Problem 5
@@ -85,11 +72,6 @@ for i in range(1, n+1):
 print "Problem 5 solution follows:"
 
 numlines = 10
-<<<<<<< HEAD
-while numlines >= 1:
-    print factorial(numlines)
-    numlines -= 1
-=======
 answer = 1
 i = numlines
 while i > 0:
@@ -98,8 +80,6 @@ while i > 0:
 	print answer
 	answer = 1
 	i -= 1
->>>>>>> all finished up~
-
 
 ###
 ### Problem 6
@@ -108,18 +88,6 @@ while i > 0:
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 6 solution follows:"
 
-<<<<<<< HEAD
-recInput = 1
-recOutput = 1
-
-# This should be easy, but I'm an idiot
-
-while recInput <= 10:
-    recOutput = recOutput + (1 // factorial(recInput))
-    print recOutput
-    recInput += 1
-
-=======
 eulersNightmare = 1
 for i in range(1, 10):
 	fact = 1
@@ -127,7 +95,7 @@ for i in range(1, 10):
 		fact *= i - j
 	eulersNightmare += 1 / float(fact)
 print eulersNightmare
->>>>>>> all finished up~
+
 
 ###
 ### Collaboration

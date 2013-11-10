53610c7165e0762abda5530992f8bea188455fad
LogTyl02 <logtyl02@evergreen.edu>
2013-11-09 21:02:15 -0800

    trying tests
    can't figure this out. fffffffff

--- homework 5/my_own_tests.py
+++ homework 5/my_own_tests.py
@@ -32,6 +32,61 @@ def state_edges(election_result_rows):
 
 #edges_2008 = state_edges(read_csv("data/2008-results.csv"))
 
-res = read_csv("data/2008-results.csv")
+#res = read_csv("data/2008-results.csv")
 
-print state_edges(read_csv("data/2008-results.csv"))
+#print state_edges(read_csv("data/2008-results.csv"))
+
+poll_rows1 = [{"ID":1, "State":"WA", "Pollster":"A", "Date":"Jan 07 2010"},
+              {"ID":2, "State":"WA", "Pollster":"B", "Date":"Mar 21 2010"},
+              {"ID":3, "State":"WA", "Pollster":"A", "Date":"Jan 08 2010"},
+              {"ID":4, "State":"OR", "Pollster":"A", "Date":"Feb 10 2010"},
+              {"ID":5, "State":"WA", "Pollster":"B", "Date":"Feb 10 2010"},
+              {"ID":6, "State":"WA", "Pollster":"B", "Date":"Mar 22 2010"}]
+
+def earlier_date(date1, date2):
+    """
+    Given two dates as strings (formatted like "Oct 06 2012"), returns True if 
+    date1 is after date2.
+    """
+    return (time.strptime(date1, "%b %d %Y") < time.strptime(date2, "%b %d %Y"))
+
+def most_recent_poll_row(poll_rows, pollster, state):
+	#for index in range(len(poll_rows)):
+		# print index, poll_rows1[index]['Date']
+		#if earlier_date(poll_rows[index]['Date'], poll_rows[index]['Date']):
+		#	print poll_rows[index]['Date']
+		#else:
+		#	print 'No'
+	recentdate = None
+	datelist = []
+	for index in range(len(poll_rows)):
+		if poll_rows[index]['State'] == state and poll_rows[index]['Pollster'] == pollster:
+			datelist.append(poll_rows[index]['Date'])
+	print datelist
+	while len(datelist) > 1:
+		if not earlier_date(datelist[0], datelist[1]):
+			datelist.remove(datelist[0])
+		else:
+			print 'Boo'
+
+def test_most_recent_poll_row():
+    assert most_recent_poll_row(poll_rows1, "A", "OR") == {"ID":4, "State":"OR", "Pollster":"A", "Date":"Feb 10 2010"}
+    assert most_recent_poll_row(poll_rows1, "A", "WA") == {"ID":3, "State":"WA", "Pollster":"A", "Date":"Jan 08 2010"}
+    assert most_recent_poll_row(poll_rows1, "B", "WA") == {"ID":6, "State":"WA", "Pollster":"B", "Date":"Mar 22 2010"}
+    assert most_recent_poll_row(poll_rows1, "B", "OR") == None
+    assert most_recent_poll_row(poll_rows1, "DoesNotExist", "DoesNotExist") == None
+
+#print poll_rows1[0]['Date']
+
+#for i in int(poll_rows1['ID']):
+#	print poll_rows1[i]['Date']
+#test_most_recent_poll_row()
+
+#print earlier_date(poll_rows1)
+
+#for index in range(len(poll_rows1)):
+	# print index, poll_rows1[index]['Date']
+	#if earlier_date(poll_rows1[index]['Date'], poll_rows1[index]['Date']):
+	#	print poll_rows1[index]['Date']
+
+most_recent_poll_row(poll_rows1, 'B', 'WA')
